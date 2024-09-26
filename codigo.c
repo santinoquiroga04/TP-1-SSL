a4c34d0377654ea40bb050ecec240b27fefdcb4c
santinoquiroga04 <sanquiroga@frba.utn.edu.ar>
2024-09-26 14:22:55 -0300

    correccion punto 2

--- codigo.c
+++ codigo.c
@@ -2,6 +2,7 @@
 /* realizar una función que reciba un carácter numérico (char) y retorne un número entero (int)*/
 
 #include <stdio.h>
+void deCharAInt(char c);
 
 int main()
 {
@@ -9,12 +10,19 @@ int main()
     printf("Ingrese un caracter numerico: ");
     scanf("%c", &c);
 
+    deCharAInt(c);
+}
+
+// Lo definidio en la funcion main debe ser una funcion aparte
+
+
+void deCharAInt(char c){
     if(c < '0' || c > '9'){
         printf("El caracter ingresado no es numerico\n");
-        return 1;
+        return;
     }
 
     char d = c - '0';
     printf("El numero entero es: %d\n", d);
-    return 0;
+    return;
 }
